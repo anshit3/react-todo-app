8fa7aa7bacf510c5459271fc42940c73a10637e3
anshit3 <dranrox@gmail.com>
2021-07-31 20:31:15 +0530

    Initial setup

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,10 @@
+import styles from './Header.module.css';
+const Header = () => (
+  <div className={styles.header}>
+    <div className={styles.header__title}>
+      <p>To-Do App</p>
+    </div>
+  </div>
+);
+
+export default Header;
