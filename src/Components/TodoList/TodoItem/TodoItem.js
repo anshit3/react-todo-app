39afb5624a2047811264e702a34d7c4e3a48b8b9
anshit3 <dranrox@gmail.com>
2021-08-01 00:39:24 +0530

    1) Add todos added and completed timestamp

--- src/Components/TodoList/TodoItem/TodoItem.js
+++ src/Components/TodoList/TodoItem/TodoItem.js
@@ -2,19 +2,34 @@ import styles from './TodoItem.module.css';
 
 const TodoItem = () => (
   <div>
-    <div className={styles.taskContainer}>
-      <input type="checkbox" name="task" id="1" />
-      <label htmlFor="1">
-        Task oneTask oneTask oneTask oneTask oneTask oneTask oneTask oneTask
-        oneTask oneTask oneTask oneTask oneTask one
-      </label>
+    <div>
+      <div className={styles.taskContainer}>
+        <input type="checkbox" name="task" id="1" />
+        <label htmlFor="1">
+          Task oneTask oneTask oneTask oneTask oneTask oneTask oneTask oneTask
+          oneTask oneTask oneTask oneTask oneTask one
+        </label>
+      </div>
+      <div className={styles.seperator}></div>
+      <div className={styles.taskTimestampContainer}>
+        <div>
+          <span>Added: </span>
+          <span>23rd Feb</span>
+        </div>
+        <div>
+          <span>Completed: </span>
+          <span>24th Feb</span>
+        </div>
+      </div>
     </div>
-    <div className={styles.taskContainer}>
-      <input type="checkbox" name="task" id="1" />
-      <label htmlFor="1">
-        Task oneTask oneTask oneTask oneTask oneTask oneTask oneTask oneTask
-        oneTask oneTask oneTask oneTask oneTask one
-      </label>
+    <div>
+      <div className={styles.taskContainer}>
+        <input type="checkbox" name="task" id="1" />
+        <label htmlFor="1">
+          Task oneTask oneTask oneTask oneTask oneTask oneTask oneTask oneTask
+          oneTask oneTask oneTask oneTask oneTask one
+        </label>
+      </div>
     </div>
   </div>
 );
