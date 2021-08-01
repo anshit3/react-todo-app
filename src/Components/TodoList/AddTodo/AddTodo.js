8eb2ad5efc949a91973054783398e0767796b1df
anshit3 <dranrox@gmail.com>
2021-08-01 15:28:39 +0530

    Add functionality to add a todo

--- src/Components/TodoList/AddTodo/AddTodo.js
+++ src/Components/TodoList/AddTodo/AddTodo.js
@@ -1,14 +1,48 @@
+import { useState } from 'react';
 import styles from './AddTodo.module.css';
 
-const AddTodo = () => (
-  <div className={styles.mainContainer}>
-    <div className={styles.mainContainer_inputTaskCtn}>
-      <input type="text" autoComplete="off" placeholder="Enter task" />
+const AddTodo = (props) => {
+  const { handleTodoAddition } = props;
+  const [todo, setTodo] = useState('');
+
+  const handlechangeOnTodoInput = (event) => {
+    setTodo(event.target.value);
+    console.log(todo);
+  };
+
+  const handleSubmit = () => {
+    let todoObj = {
+      creationTime: new Date().toDateString(),
+      completionTime: null,
+      description: todo,
+    };
+    handleTodoAddition(todoObj);
+    setTodo('');
+    console.log(todoObj);
+  };
+  return (
+    <div className={styles.mainContainer}>
+      <div className={styles.mainContainer_inputTaskCtn}>
+        <input
+          type="text"
+          onChange={handlechangeOnTodoInput}
+          autoComplete="off"
+          placeholder="Enter task"
+          value={todo}
+        />
+      </div>
+      <div className={styles.mainContainer_buttonCtn}>
+        <button
+          className={styles.mainContainer_button}
+          type="button"
+          disabled={todo.length > 0 ? false : true}
+          onClick={handleSubmit}
+        >
+          Add
+        </button>
+      </div>
     </div>
-    <div className={styles.mainContainer_buttonCtn}>
-      <button className={styles.mainContainer_button} type="button">Add</button>
-    </div>
-  </div>
-);
+  );
+};
 
 export default AddTodo;
