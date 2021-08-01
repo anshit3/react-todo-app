f078326dcc295d34c8c2ffa6fbd61ff4538c08ea
anshit3 <dranrox@gmail.com>
2021-08-01 15:32:24 +0530

    Pass data to child component to display todo

--- src/Components/TodoList/TodoList.js
+++ src/Components/TodoList/TodoList.js
@@ -1,12 +1,21 @@
+import { useState } from 'react';
 import AddTodo from './AddTodo/AddTodo';
 import TodoItem from './TodoItem/TodoItem';
 import styles from './TodoList.module.css';
 
-const TodoList = () => (
-  <div className={styles.todolistContainer}>
-    <AddTodo />
-    <TodoItem />
-  </div>
-);
+const TodoList = () => {
+  const [todos, setTodos] = useState([]);
+
+  const handleTodoAddition = (newTodo) => {
+    setTodos((prevTodos) => [newTodo, ...prevTodos]);
+  };
+
+  return (
+    <div className={styles.todolistContainer}>
+      <AddTodo handleTodoAddition={handleTodoAddition} />
+      <TodoItem allToDosObj={todos} />
+    </div>
+  );
+};
 
 export default TodoList;
